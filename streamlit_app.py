5ca4a87b1c99efd2509f31ed71b99600c65dde76
FaisalBinBander <faisalbinbander1930@gmail.com>
2025-05-01 19:41:31 +0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,29 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+import pickle
 
-st.write('Hello world!')
+import numpy as np
+
+model = pickle.load(open('trained_model.sav', 'rb') )
+
+st.title("Iris Prediction")
+
+sepal_length = st.number_input('Sepal Length')
+
+sepal_width = st.number_input('Sepal width')
+
+petal_length = st.number_input('Petal Length')
+
+petal_width = st.number_input('Petal width')
+
+ 
+
+input_data = [sepal_length,sepal_width, petal_length,   petal_width]
+
+input_data = np.asarray(input_data).reshape(1, -1)
+
+if st.button('Result'):
+
+    prediction = model.predict(input_data)
+
+    st.success(prediction[0])
